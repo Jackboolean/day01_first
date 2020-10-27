d31d7f2dfe1be32908430cb60624aad428269d69
YJJ <123456@qq.com>
2020-10-27 18:35:09 +0800

    提交了index.html部分页面模块

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,15 @@ var baseUrl='http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function(options){
     options.url=baseUrl+options.url
     //console.log(  options.url);
+
+
+//对需要权限的接口配置头信息
+//必须以my开头才行
+    if(options.url.indexOf('/my/')!==-1){
+        options.headers={
+            Authorization:localStorage.getItem('token')||""//''是啥意思？
+           }
+    }
+   
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+$(function(){
+    getUserInfo()
+})
+//获取用户信息必须封装成全局函数，写在入口函数之外
+//后需要用这个函数，所以是全局
+function getUserInfo(){
+   $.ajax({
+       url:'/my/userinfo',
+    //    headers:{
+    //     Authorization:localStorage.getItem('token')||""//''是啥意思？
+    //    },
+       success:function(res){
+       console.log(res);
+       //判断状态
+       if(res.status!==0){
+           return layui.layer.msg(res.message)
+       }
+       //请求成功,渲染用户头像
+       renderAvatar(res.data)
+       }
+   })
+}
+
+//封装用户头像渲染函数
+function renderAvatar(user){
+    //1.用户名，先渲染昵称，没有的话渲染用户名
+  var name=user.nickname||user.username
+  $('#welcome').html('欢迎&nbsp;&nbsp;'+name) 
+  //2.用户头像
+  if(user.user_pic!==null){
+      //2.1 如果头像不为空，是有头像的，则渲染头像，一旦渲染头像完成之后文本头像就不能再次使用了
+      $('.layui-nav-img').show().attr('src',user.user_pic)
+      $('.user-avater').hide()
+  } else {
+      //如果头像为空，那么文本头像显示，图片头像隐藏
+    $('.layui-nav-img').hide()
+    var text=name[0].toUpperCase()
+    $('.user-avater').show().html(text)
+  }
+}
